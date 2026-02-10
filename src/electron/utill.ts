6364e33984c65414d4d99aaa6a4bfaf61d1f6909
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-02-10 18:36:16 +0100

    improved security

--- src/electron/utill.ts
+++ src/electron/utill.ts
@@ -18,3 +18,18 @@ export function ipcWebContentsSend<Key extends keyof EventPayloadMapping>(
 ) {
     webContents.send(key, payload);
 }
+
+// Funzione per la validazione degli eventi: 
+// per prima cosa controllo se sono in devmode, se lo sono verifico che l'evento arriva dal host "localhost:5123" 
+// se non sono in devmode controllo se l'evento arriva dal index.html di dist-react, se non arriva da lì lancio un errore
+export function validateEventFrame(frame: WebFrameMain | null) {
+    if (!frame) {
+        throw new Error("frame is NULL");
+    }
+    if (isDev() && new URL(frame.url).host === "localhost:5123") {
+        return;
+    }
+    if (frame.url !== pathToFileURL(getUIPath()).toString()) {
+        throw new Error("Malicious event");
+    }
+}
